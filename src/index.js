343b2a3b492e2848378c868ebe939d5362f6d035
AlexYalovoy <YalovyyAlex@gmail.com>
2019-02-12 11:50:33 +0200

    feature/angModule add realization for directive, bootstrap, compile

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 /* eslint-disable no-empty-function */
 /* eslint-disable wrap-iife */
 (function() {
-  const directives = [];
+  const directives = {};
 
   const smallAngular = {
-    directive() {
+    directive(key, cb) {
+      if (directives[key]) {
+        directives[key].push(cb);
+        return;
+      }
 
+      directives[key] = [cb];
     },
     compile(node) {
+      const atribs = node.getAttributeNames();
 
+      atribs.forEach(atr => {
+        if (directives[atr]) {
+          directives[atr](node);
+        }
+      });
     },
     bootstrap(node) {
+      let appNode = node;
 
+      if (!node) {
+        appNode = document.querySelector('*[ng-app]');
+      }
+
+      const childNodes = appNode.querySelectorAll('*');
+      childNodes.forEach(el => {
+        this.compile(el);
+      });
     }
   };
 
@@ -19,6 +39,7 @@
   smallAngular.directive('ng-click', function(el) {});
   smallAngular.directive('ng-show', function(el) {});
   smallAngular.directive('ng-hide', function(el) {});
+  smallAngular.directive('ng-uppercase', function(el) {});
 
   window.smallAngular = smallAngular;
 })();
