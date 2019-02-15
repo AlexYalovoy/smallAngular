39f08acf32c54dba7a2514d917b88453bdc97ec1
AlexYalovoy <YalovyyAlex@gmail.com>
2019-02-15 16:05:23 +0200

    feature/directives fix review errors

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,18 @@
 /* eslint-disable no-continue */
 /* eslint-disable no-empty-function */
 /* eslint-disable wrap-iife */
+/* eslint-disable no-eval */
+/* eslint-disable no-implied-eval */
 (function() {
   const data = {};
   const scope = window;
   const watchers = [];
   const directives = {};
-  const update = [];
 
-  scope.$watch = watcher => watchers.push(watcher);
-  scope.$apply = () => watchers.forEach(cb => cb());
-  // scope.test = true;
+  scope.$watch = (checker, watcher) => watchers.push({ checker, watcher });
+  scope.$apply = () => {
+    watchers.forEach(el => el.watcher());
+  };
 
   const smallAngular = {
     get data() {
@@ -37,8 +39,7 @@
 
       atribs.forEach(atr => {
         if (directives[atr]) {
-          const value = node.getAttribute(atr);
-          directives[atr].forEach(cb => cb({ scope, node, value, restAtrs }));
+          directives[atr].forEach(cb => cb(scope, node, restAtrs));
         }
       });
     },
@@ -57,39 +58,45 @@
     }
   };
 
-  smallAngular.directive('ng-model', function({ scope, node, value }) {
+  smallAngular.directive('ng-model', function(scope, node) {
+    const value = node.getAttribute('ng-model');
     node.value = eval(value);
 
     node.addEventListener('input', event => {
       scope[value] = event.target.value;
       scope.$apply();
     });
-    scope.$watch(() => (node.value = eval(value)));
+    scope.$watch(() => eval(node.getAttribute('ng-model')), () => (node.value = eval(value)));
   });
 
-  smallAngular.directive('ng-show', function({ scope, node, value }) {
+  smallAngular.directive('ng-show', function(scope, node) {
+    const value = node.getAttribute('ng-show');
     node.style.display = eval(value) ? 'block' : 'none';
-    scope.$watch(() => (node.style.display = eval(value) ? 'block' : 'none'));
+    scope.$watch(() => eval(node.getAttribute('ng-show')), () => (node.style.display = eval(value) ? 'block' : 'none'));
     scope.$apply();
   });
 
-  smallAngular.directive('ng-bind', function({ scope, node, value }) {
-    scope.$watch(() => (node.innerText = eval(value)));
+  smallAngular.directive('ng-bind', function(scope, node) {
+    const value = node.getAttribute('ng-bind');
+    scope.$watch(() => eval(node.getAttribute('ng-bind')), () => (node.innerText = eval(value)));
   });
 
-  smallAngular.directive('ng-hide', function({ scope, node, value }) {
+  smallAngular.directive('ng-hide', function(scope, node) {
+    const value = node.getAttribute('ng-hide');
     node.style.display = eval(value) ? 'none' : 'block';
-    scope.$watch(() => (node.style.display = eval(value) ? 'none' : 'block'));
+    scope.$watch(() => eval(node.getAttribute('ng-hide')), () => (node.style.display = eval(value) ? 'none' : 'block'));
   });
 
-  smallAngular.directive('ng-click', function({ scope, node, value }) {
+  smallAngular.directive('ng-click', function(scope, node) {
+    const value = node.getAttribute('ng-click');
     node.addEventListener('click', () => {
       eval(value);
       scope.$apply();
     });
   });
 
-  smallAngular.directive('ng-repeat', function({ scope, node, value }) {
+  smallAngular.directive('ng-repeat', function(scope, node) {
+    const value = node.getAttribute('ng-repeat');
     function appendFromIterable(node, iterable) {
       const currEl = node.nextSibling;
       node.innerText = '';
@@ -110,7 +117,7 @@
     const endEl = node.nextSibling;
     appendFromIterable(node, iterable);
 
-    scope.$watch(() => {
+    scope.$watch(() => eval(value.split('in')[1].trim()), () => {
       const iterable = eval(value.split('in')[1].trim());
       let currEl = node.nextSibling;
 
@@ -125,15 +132,15 @@
     });
   });
 
-  smallAngular.directive('ng-uppercase', function({ scope, node, value }) {
+  smallAngular.directive('ng-uppercase', function(scope, node) {
     node.style.textTransform = 'uppercase';
   });
 
-  smallAngular.directive('ng-make-short', function({ scope, node, value, restAtrs }) {
+  smallAngular.directive('ng-make-short', function(scope, node, restAtrs) {
     node.innerText = `${node.innerText.slice(0, restAtrs['length'] || 5)}...`;
   });
 
-  smallAngular.directive('ng-random-color', function({ scope, node, value }) {
+  smallAngular.directive('ng-random-color', function(scope, node) {
     const getRandCol = () => Math.floor(Math.random() * 256);
     node.style.backgroundColor = `rgb(${getRandCol()}, ${getRandCol()}, ${getRandCol()})`;
 
@@ -141,7 +148,8 @@
       `rgb(${getRandCol()}, ${getRandCol()}, ${getRandCol()})`));
   });
 
-  smallAngular.directive('ng-init', function({ scope, node, value }) {
+  smallAngular.directive('ng-init', function(scope, node) {
+    const value = node.getAttribute('ng-init');
     eval(value);
   });
 
